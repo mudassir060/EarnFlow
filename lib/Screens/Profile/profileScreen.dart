6cfe111c0982b8df2ef1e103f39be88d780159ab
mudassir060 <mudassirmukhtar4@gmail.com>
2023-04-24 21:36:28 +0500

    profile image

--- lib/Screens/Profile/profileScreen.dart
+++ lib/Screens/Profile/profileScreen.dart
@@ -1,6 +1,8 @@
 import 'package:earnflow/constant/style.dart';
+import 'package:earnflow/widgets/myspacer.dart';
 import 'package:flutter/material.dart';
 
+import '../../constant/Icons.dart';
 import '../../constant/images.dart';
 
 class profileScreen extends StatefulWidget {
@@ -17,38 +19,78 @@ class _profileScreenState extends State<profileScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Stack(
-                children: [
-                  const CircleAvatar(
+            myspacer(60.0, 0.0),
+            Stack(
+              children: [
+                const CircleAvatar(
+                  radius: 55,
+                  child: CircleAvatar(
                     radius: 55,
-                    child: CircleAvatar(
-                      radius: 55,
-                      backgroundImage: AssetImage(profile),
-                    ),
+                    backgroundImage: AssetImage(profile),
                   ),
-                  Positioned(
-                    bottom: 3,
-                    right: 2,
-                    child: CircleAvatar(
-                      radius: 20,
-                      backgroundColor: primaryColor,
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.camera_alt,
-                        ),
+                ),
+                Positioned(
+                  bottom: 3,
+                  right: 2,
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: primaryColor,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.camera_alt,
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            const Center(child: Text("profileScreen")),
+            myspacer(10.0, 0.0),
+            const Text("Abc"),
+            const Text("Abc@gmail.com"),
+            myspacer(20.0, 0.0),
+            profileList("Contact us", contactP, () {}),
+            profileList("Privacy Policy", privacy, () {}),
+            profileList("White paper", whitepaper, () {}),
+            profileList("Rate us", rateus, () {}),
+            myspacer(30.0, 0.0),
+            const Text("Social Links"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  twitter,
+                  width: 100,
+                ),
+                Image.asset(
+                  facebook,
+                  width: 100,
+                ),
+                Image.asset(
+                  discord,
+                  width: 100,
+                ),
+              ],
+            )
           ],
         ),
       ),
     );
   }
+}
+
+Widget profileList(title, img, fun) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 4.0, right: 4, bottom: 4),
+    child: Card(
+      child: ListTile(
+        leading: Image.asset(
+          img,
+        ),
+        title: Text(title),
+        trailing: IconButton(
+            onPressed: fun, icon: const Icon(Icons.arrow_forward_ios)),
+      ),
+    ),
+  );
 }
---
+++ lib/constant/Icons.dart
@@ -0,0 +1,18 @@
+const String discord = "assets/Icons/discord.png";
+const String facebook = "assets/Icons/facebook.png";
+const String twitter = "assets/Icons/twitter.png";
+const String contactP = "assets/Icons/contact 1.png";
+const String privacy = "assets/Icons/privacy.png";
+const String whitepaper = "assets/Icons/whitepaper.png";
+const String rateus = "assets/Icons/rateus.png";
+
+
+
+// const String rocket = "assets\Icons\dashboard.png";
+// const String rocket = "";
+// const String rocket = "";
+// const String rocket = "";
+// const String rocket = "";
+// const String rocket = "";
+// const String rocket = "";
+// const String rocket = "";
